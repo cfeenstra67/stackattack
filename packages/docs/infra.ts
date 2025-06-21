d918408031700d9b627dd10c18f2d333043519f2
cfeenstra67 <cameron.l.feenstra@gmail.com>
2025-06-21 13:42:44 -0700

    add license

--- packages/docs/infra.ts
+++ packages/docs/infra.ts
@@ -1,29 +1,29 @@
-import * as pulumi from '@pulumi/pulumi';
-import * as saws from '@stackattack/aws';
-import shared from 'shared-infra';
+import * as pulumi from "@pulumi/pulumi";
+import * as saws from "@stackattack/aws";
+import shared from "shared-infra";
 
 export default () => {
   const ctx = saws.context();
   const config = new pulumi.Config();
 
-  const domainName = config.require('domain-name');
-  const sharedStack = config.require('shared-stack');
+  const domainName = config.require("domain-name");
+  const sharedStack = config.require("shared-stack");
 
   const sharedRef = saws.stackRef(sharedStack, shared);
-  const certificate = sharedRef.require('camFeenstraEastCertificate');
+  const certificate = sharedRef.require("camFeenstraEastCertificate");
 
   const bucket = saws.bucket(ctx);
-  
+
   saws.bucketDirectory(ctx, {
     bucket,
-    directory: './dist',
+    directory: "./dist",
   });
 
   saws.staticSite(ctx, {
     bucket,
     domain: domainName,
     adapter: saws.astroAdapter(),
-    certificate
+    certificate,
   });
 
   return { url: `https://${domainName}` };
